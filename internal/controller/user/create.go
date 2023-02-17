19d963dbff3aa08ea929bc04e0a138f67c3477b3
Illus1onnn <1161524405@qq.com>
2023-02-18 01:52:34 +0800

    feat: 测试路由控制器组合

--- internal/controller/user/create.go
+++ internal/controller/user/create.go
@@ -1,11 +1,14 @@
 package user
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/valyevo/gosimple/internal/pkg/core"
 )
 
 func (ctrl *userController) Create(ctx *gin.Context) {
 	err := ctrl.b.User().Create()
+	fmt.Println(err)
 	core.WriteResponse(ctx, err, nil)
 }
--- internal/routes/api.go
+++ internal/routes/api.go
@@ -3,8 +3,10 @@ package routes
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/valyevo/gosimple/internal/controller/user"
 	"github.com/valyevo/gosimple/internal/pkg/core"
 	"github.com/valyevo/gosimple/internal/pkg/errcode"
+	"github.com/valyevo/gosimple/internal/store"
 )
 
 type RouteFunc func(app *gin.RouterGroup)
@@ -28,4 +30,12 @@ func ApiRoutes(app *gin.RouterGroup) {
 
 		core.WriteResponse(ctx, errcode.OK, data)
 	})
+
+	// User 注册用户路由
+	userCtrl := user.New(store.D)
+	userGrp := app.Group("/user")
+	{
+		// 创建用户
+		userGrp.GET("", userCtrl.Create)
+	}
 }
