bca5fe441f56627be4cb6f26f8915873f88941f6
Illus1onnn <1161524405@qq.com>
2023-02-18 01:12:39 +0800

    feat: User biz层实现

---
+++ internal/controller/biz/user/user.go
@@ -0,0 +1,29 @@
+package user
+
+import "github.com/valyevo/gosimple/internal/store"
+
+// UserBiz 定义 User 业务模块实现的方法
+type UserBiz interface {
+	Create() // 创建用户
+	Index()  // 用户列表
+}
+
+// userBiz UserBiz 的具体实现
+type userBiz struct {
+	store store.IStore
+}
+
+// NewUserBiz 实例化 UserBiz 接口实例
+func NewUserBiz(store store.IStore) *userBiz {
+	return &userBiz{store}
+}
+
+// Create 是 UserBiz 接口 `Create` 方法具体实现
+func (b *userBiz) Create() {
+
+}
+
+// Index 是 UserBiz 接口 `Index` 方法具体实现
+func (b *userBiz) Index() {
+
+}
