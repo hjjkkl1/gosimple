da5fab2363abc77af1d3e27d6b773a5ae1ea9090
Illus1onnn <1161524405@qq.com>
2023-02-18 12:06:47 +0800

    feat: user请求验证结构体

---
+++ internal/request/user.go
@@ -0,0 +1,6 @@
+package request
+
+type CreateUserRequest struct {
+	Username string `json:"username" valid:"alphanum,required,stringlength(1|255)"`
+	Password string `json:"password" valid:"required,stringlength(6|18)"`
+}
