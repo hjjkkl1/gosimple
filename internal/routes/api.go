4cb7f46798603da52e6b632818c0ae5183a63279
Illus1onnn <1161524405@qq.com>
2023-02-18 13:30:26 +0800

    fix: mlogin -> /login/mobile

--- internal/routes/api.go
+++ internal/routes/api.go
@@ -37,7 +37,7 @@ func ApiRoutes(app *gin.RouterGroup) {
 	loginCtrl := login.NewLoginController(store.D)
 	authGrp := app.Group("/auth")
 	{
-		authGrp.POST("/mlogin", loginCtrl.Mobile)
+		authGrp.POST("/login/mobile", loginCtrl.Mobile)
 	}
 
 	// User 注册用户路由
