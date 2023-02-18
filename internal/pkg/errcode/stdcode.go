27fea949454b071253e20ac384ac8025f60c2539
Illus1onnn <1161524405@qq.com>
2023-02-18 12:38:18 +0800

    feat: 新增错误码

--- internal/pkg/errcode/stdcode.go
+++ internal/pkg/errcode/stdcode.go
@@ -4,7 +4,9 @@ const defref = "https://github.com/valyevo/gosimple/tree/main/docs"
 const nullref = "无"
 
 var (
-	OK          = new(200, "0", "ok", nullref)
-	ServerError = new(500, "100001", "服务器内部错误", nullref)
-	ErrNotFound = new(404, "100002", "找不到资源", nullref)
+	OK                  = new(200, "0", "ok", nullref)
+	ServerError         = new(500, "100001", "服务器内部错误", nullref)
+	ErrNotFound         = new(404, "100002", "找不到资源", nullref)
+	ErrBind             = new(400, "100003", "缺省参数", defref)
+	ErrInvalidParameter = new(400, "100003", "无效的参数", defref)
 )
